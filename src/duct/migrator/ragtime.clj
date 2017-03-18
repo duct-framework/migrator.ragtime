f8590ed5955d45715b47f321bda0d85c24d09371
James Reeves <jreeves@weavejester.com>
2017-03-18 18:50:32 +0000

    Initial commit

---
+++ src/duct/migrator/ragtime.clj
@@ -0,0 +1,6 @@
+(ns duct.migrator.ragtime)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/duct/migrator/ragtime_test.clj
@@ -0,0 +1,7 @@
+(ns duct.migrator.ragtime-test
+  (:require [clojure.test :refer :all]
+            [duct.migrator.ragtime :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
