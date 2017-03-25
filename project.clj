848a29af49ae4c89246a7cbe3bfa8ade12bf8b3d
James Reeves <jreeves@weavejester.com>
2017-03-25 15:40:20 +0100

    Add :duct.migrator.ragtime/sql key

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [integrant "0.3.0"]
+                 [pandect "0.6.1"]
                  [ragtime "0.6.4"]])
--- src/duct/migrator/ragtime.clj
+++ src/duct/migrator/ragtime.clj
@@ -10,8 +10,25 @@
    :raise-error strategy/raise-error
    :rebase      strategy/rebase})
 
+(def ^:private colon (.getBytes ":" "US-ASCII"))
+(def ^:private comma (.getBytes "," "US-ASCII"))
+
+(defn- netstring [bs]
+  (let [size (.getBytes (str (count bs)) "US-ASCII")]
+    (byte-array (concat size colon bs comma))))
+
+(defn- hash-migration [{:keys [up down]}]
+  (sha1 (byte-array (concat (netstring (.getBytes up))
+                            (netstring (.getBytes down))))))
+
+(defn- generate-id [key opts]
+  (str (:id opts key) ":" (subs (hash-migration opts) 0 8)))
+
 (defmethod ig/init-key :duct.migrator/ragtime
   [_ {:keys [database strategy migrations] :or {strategy :raise-error}}]
   (repl/migrate {:datastore  (jdbc/sql-database (:spec database))
                  :migrations migrations
                  :strategy   (strategies strategy)}))
+
+(defmethod ig/init-key ::sql [key opts]
+  (jdbc/sql-migration (assoc opts :id (generate-id key opts))))
