1893cf662e6cdd23f93d5ddca2a30fcff0e189b5
James Reeves <jreeves@weavejester.com>
2017-11-03 00:54:00 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/duct-framework/migrator.ragtime"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [duct/core "0.6.0"]
-                 [duct/logger "0.1.1"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+                 [duct/core "0.6.1"]
+                 [duct/logger "0.2.1"]
                  [integrant "0.6.1"]
                  [pandect "0.6.1"]
-                 [ragtime "0.7.1"]]
+                 [ragtime "0.7.2"]]
   :profiles
   {:dev {:dependencies [[duct/database.sql "0.1.0"]
-                        [org.clojure/java.jdbc "0.7.0"]
-                        [org.xerial/sqlite-jdbc "3.20.0"]]}})
+                        [org.clojure/java.jdbc "0.7.3"]
+                        [org.xerial/sqlite-jdbc "3.20.1"]]}})
