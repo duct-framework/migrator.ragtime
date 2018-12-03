3ae1bdc63ba2f704c3a2b8c6bb2ed83321b99cfc
James Reeves <jreeves@weavejester.com>
2018-12-03 10:03:42 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [duct/core "0.7.0-beta2"]
-                 [duct/logger "0.2.1"]
+                 [duct/logger "0.3.0"]
                  [integrant "0.7.0"]
                  [pandect "0.6.1"]
                  [ragtime "0.7.2"]]
   :profiles
   {:dev {:dependencies [[duct/database.sql "0.1.0"]
-                        [org.clojure/java.jdbc "0.7.6"]
-                        [org.xerial/sqlite-jdbc "3.21.0.1"]]}})
+                        [org.clojure/java.jdbc "0.7.8"]
+                        [org.xerial/sqlite-jdbc "3.25.2"]]}})
