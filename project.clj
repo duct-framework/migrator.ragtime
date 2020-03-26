1a4cb532b569c01a77a53a09e310d6f1bf417883
James Reeves <jreeves@weavejester.com>
2020-03-26 15:41:33 +0000

    Release 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/migrator.ragtime "0.3.1"
+(defproject duct/migrator.ragtime "0.3.2"
   :description "Integrant methods for running database migrations using Ragtime"
   :url "https://github.com/duct-framework/migrator.ragtime"
   :license {:name "Eclipse Public License"
